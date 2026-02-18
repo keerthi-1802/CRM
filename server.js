cc06acf319cd7040e65ecddc70eaa8db707d130a
Kiruthika Narayanan <kiruthikanarayanan002@gmail.com>
2026-02-18 11:25:20 +0530

    fix render port

--- server.js
+++ server.js
@@ -31,7 +31,8 @@
 
   // initSocket(server);
 
-  const PORT = 3000;
+  // const PORT = 3000;
+  const PORT = process.env.PORT || 3000;
  server.listen(PORT, () => {
   console.log(`Server running: http://localhost:${PORT}`);
 });
